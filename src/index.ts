baafa120e6c98916c3a36b7d59767959cd3cfee1
Brandon Gardner <brandon.gardner2@outlook.com>
2019-12-31 15:03:55 -0600

    renders an empty map

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import User from './models/User';
 import Company from './models/Company';
 
-console.log('hello');
+const mapElem = document.getElementById('map');
+const map = new google.maps.Map(mapElem, {
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+  zoom: 1,
+});

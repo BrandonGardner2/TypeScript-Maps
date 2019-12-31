502d2e616ba68dbee29083c2a614eaa44ff09bc9
Brandon Gardner <brandon.gardner2@outlook.com>
2019-12-31 15:17:19 -0600

    moves google map to private file

---
+++ src/models/Map.ts
@@ -0,0 +1,16 @@
+class GoogleMap {
+  private googleMap: google.maps.Map;
+
+  constructor() {
+    this.googleMap = new google.maps.Map(
+      document.getElementById('map'), {
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+      zoom: 1,
+    });
+  }
+}
+
+export default GoogleMap;

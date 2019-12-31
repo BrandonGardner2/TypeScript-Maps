195dc3cf1b76e9fdf53efeb27b85649b31c2d78e
Brandon Gardner <brandon.gardner2@outlook.com>
2019-12-31 15:17:55 -0600

    removes old map code from index.ts and replaces with class constructor

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,4 @@ import User from './models/User';
 import Company from './models/Company';
 import GoogleMap from './models/Map';
 
-const mapElem = document.getElementById('map');
-const map = new google.maps.Map(mapElem, {
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-  zoom: 1,
-});
+const map = new GoogleMap();
